2d81689c2ffd9f464f15ab6e57eb029d60f11921
William Woodard <williamwoodard98@gmail.com>
2024-02-23 10:22:43 -0600

    starting to list fish out

---
+++ scripts/database.js
@@ -0,0 +1,314 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const database = {
+    fish: [
+        {
+            id: 1,
+            image: "#",
+            name: "Gil",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 2,
+            image: "#",
+            name: "Nemo",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 3,
+            image: "#",
+            name: "Dory",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 4,
+            image: "#",
+            name: "Bubbles",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 5,
+            image: "#",
+            name: "Splash",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 6,
+            image: "#",
+            name: "Finley",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 7,
+            image: "#",
+            name: "Coral",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 8,
+            image: "#",
+            name: "Aqua",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 9,
+            image: "#",
+            name: "Sushi",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 10,
+            image: "#",
+            name: "Marlen",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 11,
+            image: "#",
+            name: "Bubbly",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 12,
+            image: "#",
+            name: "Flounder",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 13,
+            image: "#",
+            name: "Splashy",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 14,
+            image: "#",
+            name: "Tim",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 15,
+            image: "#",
+            name: "Tom",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 16,
+            image: "#",
+            name: "Poseidon",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 17,
+            image: "#",
+            name: "Blinky",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 18,
+            image: "#",
+            name: "Rainbow",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 19,
+            image: "#",
+            name: "Seabiscuit",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+        {
+            id: 20,
+            image: "#",
+            name: "Goldie",
+            species: "Flounder",
+            food: "crustaceans",
+            size: 3,
+            location: "Panama",
+            quote: "I love Panama"
+        },
+    ]
+}
+
+/* 
+   
+    Splasher
+    Squirt
+    Starfish
+    Seashell
+    Whiskers
+    Tidal
+    Sandy
+    Pebbles
+    Tropicana
+    Reef
+    Surge
+    Blu
+    Neptune
+    Captain
+    Coraline
+    Shimmer
+    Seafarer
+    Scallop
+    Sandy
+    Finn
+    Triton
+    Seaweed
+    Splasher
+    Breezy
+    Tide
+    Flicker
+    Bubbler
+    Oceanus
+    Splashy
+    Swimmer
+    Aqua
+    Echo
+    Coral
+    Seabreeze
+    Siren
+    Bubbles
+    Splasher
+    Pearl
+    Poseidon
+    Shelly
+    Bubbles
+    Splashy
+    Blue
+    Tidal
+    Marlin
+    Coral
+    Splasher
+    Seashine
+    Sandy
+    Tropicana
+    Reef
+    Seashell
+    Starfish
+    Seabiscuit
+    Goldie
+    Splasher
+    Bubbles
+    Neptune
+    Poseidon
+    Blinky
+    Rainbow
+    Seabreeze
+    Splashy
+    Bubbles
+    Blu
+    Seafarer
+    Sandy
+    Coraline
+    Shimmer
+    Scallop
+    Sandy
+    Pebbles
+    Tropicana
+    Finn
+    Triton
+    Seaweed
+    Splasher
+    Breezy
+    Tide
+    Oceanus
+    Splashy
+    Swimmer
+    Aqua
+    Echo
+    Coral
+    Seabreeze
+    Siren
+    Bubbles
+    Splasher
+    Pearl
+    Poseidon
+    Shelly
+    Bubbles
+    Splashy
+    Blue
+    Tidal
+    Marlin
+    Coral
+    Splasher
+    Seashine
+*/
